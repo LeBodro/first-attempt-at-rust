6806f620e8af494674bdeb48653700bcbf7458d4
Boud <maxime.boudreault.0@gmail.com>
2019-04-20 20:20:18 -0400

    Make it a 'guess the number' game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    let stdout = stdout();
-    let out = b"Hello Apos!";
-    let width = 11;
+    let str_instruction = "Guess the number (1-100)!";
+    let str_too_low = "Too low. Try again.";
+    let str_too_high = "Calm down! That guess is too high!";
+    let str_victory = "Congratulations! You guessed right!";
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    cls();
+    println!("{:?}", str_instruction);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        cls();
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{:?}", str_too_low),
+            Ordering::Greater => println!("{:?}", str_too_high),
+            Ordering::Equal => {
+                println!("{:?}", str_victory);
+                break;
+            }
+        }
+    }
+}
+
+fn cls() {
+    print!("{}[2J", 27 as char);
 }
